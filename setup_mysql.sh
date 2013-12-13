5fd2434f8bd9cb3607aba1fdc3879e92dfa75f2d
Rahul Patil <root@centos.hellix.hell>
2013-12-13 23:27:33 +0530

    updated script.. still pending in creating new init script

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -18,17 +18,22 @@ package_manager=$( [[ -x $(command -v yum) ]] && echo "RHEL Based" || echo "Ubun
 
 
 rhel_installation(){
-	[[ -x $(command -v mysqld)]]
-        yum install -y mysql-server mysql.$arch
-        mysqladmin status || /etc/init.d/mysqld restart
-        eval $set_password
+        rpm -qa mysql-server* | grep -qw mysql-server || 
+	{ yum install -y mysql-server mysql.$arch;
+	  mysqladmin status || /etc/init.d/mysqld restart;
+          eval $set_password; 
+	}
 }
 
 ubunt_installation(){
         export DEBIAN_FRONTEND=noninteractive
-        apt-get -q -y install mysql-server
-        mysqladmin status || /etc/init.d/mysqld restart
-        eval $set_password
+
+	dpkg -l | grep -qw mysql-server ||
+        { 
+	 apt-get -q -y install mysql-server;
+	 mysqladmin status || /etc/init.d/mysqld restart;
+         eval $set_password;
+	}
 }
 
 install_packages() {
@@ -38,21 +43,43 @@ install_packages() {
         esac
 }
 
-
-setup_new_instance(){
-
-	echo  "MySQL Successfully Installed"
+ask() { 
 	while true
 	do
 		read -p "Do you want to add more Instance (y/n)?" ans
 		case $ans in 
 			[yY]|[yY][eE][sS]) 	ans=yes; break ;;
-			[nN]|[nN][oO])		ans=no; break ;;
+			[nN]|[nN][oO])		ans=no;  exit 0 ;;
 					*)	echo "Please Press (y/n)" ;;
 		esac
 	done
+}
+
+setup_new_instance(){
+
+	echo  "MySQL Successfully Installed"
+	ask
+	total_instance=$( ls -1d /var/lib/mysql* | wc -l )
+	(( total_instance++ ))
+	i=total_instance
+	port=$[ 3306 + $i ]
+	location=/var/lib/mysql
+	logpath=/var/log/mysql${i}
+	[[ -d ${location}${i} ]] && { echo "Already Exists"; exit 1; }
+	mkdir ${location}${i}
+	mkdir /var/log/mysql${i}
+	chown -R mysql. ${location}${i} $logpath
+	cnf=/etc/my${i}.cnf
+	cp /etc/init.d/mysql /etc/init.d/mysql${i}
 	
-	echo $ans
+	cp /etc/my.cnf $cnf
+	cd /etc/mysql${i}
+	sed -i "s/3306/$port/g" $cnf
+	sed -i "s/mysqld.sock/mysqld$i.sock/g" $cnf
+	sed -i "s/mysqld.pid/mysqld$i.pid/g" $cnf
+	sed -i "s/var\/lib\/mysql/var\/lib\/mysql$i/g" $cnf
+	sed -i "s/var\/log\/mysql/var\/log\/mysql$i/g" $cnf
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql$i/	
 
 }
 
