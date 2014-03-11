38cea32480c484ae16e4eb5274ccde745b442dba
Rahul Patil <loginrahul90@gmail.com>
2014-03-11 22:58:46 +0530

    Create sys_info.sh

---
+++ sys_info.sh
@@ -0,0 +1,16 @@
+awk 'BEGIN {
+	while("df -hP " | getline) {
+		if ( $NF == "/" ) {
+			printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5
+		}
+	}
+	while( getline cpu < "/proc/loadavg" ) {
+		printf "CPU Load: %.2f\n", $(NF-2)
+	}
+	
+	while( "free -m"| getline) {
+		if( $0 ~ /Mem:/) {
+		printf "Memory Usage: %s/%sMB (%.2f%)\n", $3,$2,$3*100/$2
+		}
+	}
+}'
