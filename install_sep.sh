1254ac8bf7d75fa17d860654f956be8b6e36006b
Rahul Patil <loginrahul90@gmail.com>
2016-07-14 23:47:35 +0530

    Update install_sep.sh

--- install_sep.sh
+++ install_sep.sh
@@ -2,8 +2,16 @@
 
 
 ## Global vars
-sep_url='http://path/SymantecEndpointProtection.zip'
+sep_url='http://localhost/SymantecEndpointProtection.zip'
 pkgs=( wget unzip )
+sep_tmp=/tmp/sep/
+
+## RHEL
+java_url=http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.rpm
+java_home=/usr/java/jdk1.7.0_71
+
+## DEB
+java_home_deb=/usr/lib/jvm/java-7-openjdk-amd64/jre/
 
 info(){
 
@@ -98,39 +106,72 @@ redhat(){
     then
        warn "Java already installed"
     else
-       if [[ ! -f /opt/jdk-7u71-linux-x64.rpm ]]
+       if [[ ! -f /opt/$( basename $java_url ) ]]
        then
            wget -q -O /opt/jdk-7u71-linux-x64.rpm \
                --no-check-certificate --no-cookies \
                --header 'Cookie: oraclelicense=accept-securebackup-cookie' \
-               http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.rpm
+               $java_url 
        fi
-       rpm -Uvh /opt/jdk-7u71-linux-x64.rpm
-       printf 'export JAVA_HOME=/usr/java/jdk1.7.0_71/\nexport PATH=$PATH:/usr/java/jdk1.7.0_71/\n' \
+       rpm -Uvh /opt/$( basename $java_url )
+       printf "export JAVA_HOME=/usr/java/jdk1.7.0_71/\nexport PATH=\$PATH:$java_home/bin/\n" \
           > /etc/profile.d/java.sh
 
     fi
+}
 
+
+sep(){
     info "Downloading sementec antivirus"
+    sep_file=SymantecEndpointProtection.zip
+    [[ -d $sep_home ]] || mkdir $sep_home
+    cd $sep_home
+    if [[ ! -f $sep_file ]]
+    then
+        wget -cnd $sep_url
+    fi
+    [[ ! -d Configuration ]] && unzip SymantecEndpointProtection.zip
+    bash ./install.sh
+}
 
-    cd /opt/
-    #if [[ ! -f SymantecEndpointProtection.zip ]]
-    #then
-    #    wget -cnd $sep_url
-    #fi
+ubuntu(){
 
+    info "Installing deps"
 
+    for pkg in ${pkgs[@]}
+    do
+       info "Installing deps :: $pkg"
+       if ! dpkg -l | grep -q $pkg
+       then
+           apt-get install -y $pkg
+           success "Installed :: $pkg"
+       else
+           warn "Already Installed :: $pkg"
+       fi
+    done
 
+    info "Installing Java"
+    if type -p java >/dev/null 2>&1
+    then
+       warn "Java already installed"
+    else
+       apt-get install openjdk-7-jdk -y 
+       printf "export JAVA_HOME=$java_home/\nexport PATH=\$PATH:$java_home_deb/bin\n" \
+          > /etc/profile.d/java.sh
+       sudo update-alternatives --install /usr/bin/java java $java_home_deb/bin java 1
+    
+    fi
 
+    sep
 }
 
 main(){
-
+   [[ $UID -ne 0 ]] && { echo "You must be root"; exit 1; };
    detect_os
    detect_os_type
    case $_OS in
       redhat) redhat ;;
-      ubuntu) echo ubuntu ;;
+      ubuntu) ubuntu ;;
            *) fail "unkown OS"; exit 1 ;;
     esac
 }
