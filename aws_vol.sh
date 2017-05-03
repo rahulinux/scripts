e1e81240aa33531978038b141093c86767805976
Rahul Patil <loginrahul90@gmail.com>
2017-05-03 11:27:24 +0200

    Update aws_vol.sh

--- aws_vol.sh
+++ aws_vol.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Create and Attach Volume to Instace using IP Address 
+set -e
+# Create and Attach Volume to Instace using IP Address
 
-AWS_ACCESS_KEY_ID=XXXX
-AWS_SECRET_ACCESS_KEY=XXXX
-REGION=us-east-1
-AWS_DEFAULT_REGION=$REGION
-VPC=VPC_ID
+export AWS_ACCESS_KEY_ID=XXX
+export AWS_SECRET_ACCESS_KEY=XXXXXX
+export REGION=us-east-1
+export AWS_DEFAULT_REGION=$REGION
 
-if [[ $# -ne 1 ]]
-then 
-    echo "Usage: $0 <hostname|IP>"
-    exit 1 
+if [[ $# -ne 2 ]]
+then
+    echo "Usage: $0 <hostname|IP> <size>"
+    exit 1
 fi
 
 msg(){
@@ -24,15 +24,15 @@ get_instance_data(){
     # returns ZONE STATUS IP INSTANCE_ID DEVICE
     local host=$1
     if ! echo $host | grep -qE '\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b'
-    then 
-         host=$(dig +short $host) 
+    then
+         host=$(dig +short $host)
     fi
     raw_data="$(aws ec2 describe-instances \
         --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name,PrivateIpAddress,InstanceId,BlockDeviceMappings[*].DeviceName]' \
-        --output text | grep -A1 "${host}\t")" 
+        --output text | grep -A1 "${host}\t")"
     instance_data="$( echo "${raw_data}" | head -1 )"
     vid=$( echo "${raw_data}" | grep -o 'sd.' | sort | uniq | tail -1 | cut -c 3- | tr "0-9a-z" "1-9a-z_")
-    echo "${instance_data}" "/dev/sd${vid}" 
+    echo "${instance_data}" "/dev/sd${vid}"
 }
 
 msg "Fetching Instance Data"
@@ -40,7 +40,7 @@ read ZONE STATUS IP INSTANCE_ID DEVICE <<<"$(get_instance_data $1)"
 msg "Done"
 
 msg "Creating Volume in Zone $ZONE for IP $IP [ID: $INSTANCE_ID]"
-volume_id="$(aws ec2 create-volume --size 20 --region $REGION --availability-zone $ZONE --volume-type standard | jq -r .VolumeId)"
+volume_id="$(aws ec2 create-volume --size ${2:-10} --region $REGION --availability-zone $ZONE --volume-type standard | jq -r .VolumeId)"
 
 msg "Volume has been created with ID:$volume_id"
 echo "Waiting for Volume availability" && sleep 30
