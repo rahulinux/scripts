07ba7a0baa6e730d71b7a16b735bea8717e04949
Rahul Patil <loginrahul90@gmail.com>
2017-11-21 17:40:13 +0100

    Update start_minikube.sh

--- start_minikube.sh
+++ start_minikube.sh
@@ -1,2 +1,2 @@
-minikube start  --kubernetes-version v1.7.4 --extra-config=apiserver.Features.EnableSwaggerUI=true --cpus 4 --memory 8192
+minikube start  --kubernetes-version v1.7.4 --extra-config=apiserver.Features.EnableSwaggerUI=true --cpus 4 --memory 8192 --extra-config=apiserver.GenericServerRunOptions.AuthorizationMode=RBAC --extra-config=apiserver.GenericServerRunOptions.AuthorizationRBAC,SuperUser=minikube
  
